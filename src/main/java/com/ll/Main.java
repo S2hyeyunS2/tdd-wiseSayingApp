b88a106bc7bb3fb510f16b448d4d0aa68f068c24
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-11-29 11:37:04 +0900

    1단계 완료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -13,6 +15,7 @@ public class Main {
 
     private static void lab1() {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("명령) ");
         String cmd= scanner.nextLine().trim();
 
@@ -21,7 +24,19 @@ public class Main {
     }
 
     private static void lab2() {
+        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
 
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령) ");
+        String cmd= scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " +cmd);
+
+        System.out.println("명령) ");
+        cmd= scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " +cmd);
     }
 
 }
