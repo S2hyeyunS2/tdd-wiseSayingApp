ffcf6d0923499be9630bb38923e6217a9e66e813
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-11-29 16:38:02 +0900

    3강까지

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,17 +1,17 @@
 package com.ll;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        lab1();
-        lab2();
+        // lab1();
+        // lab2();
+        //lab3();
+         lab4();
     }
-
 
     private static void lab1() {
         Scanner scanner = new Scanner(System.in);
@@ -39,4 +39,37 @@ public class Main {
         System.out.println("입력한 명령: " +cmd);
     }
 
+    private static void lab3() {
+        System.out.println("== 시작 ==");
+
+        System.out.println("안녕하세요.");
+        System.out.println("반갑습니다.");
+
+        System.out.println("== 끝 ==");
+    }
+
+    private static void lab4(){
+        System.out.println("== 시작 ==");
+
+        //출력이 모니터로 안되도록 설정
+        ByteArrayOutputStream output=new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕하세요.");
+        System.out.println("반갑습니다.");
+
+        //다시 모니터로 출력되도록 설정
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+        System.out.println("== 끝 ==");
+
+        System.out.println("출력 : " +output.toString());
+
+        //output 자원 해제
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
