8d60003be852c133c280e5db564ea21a51b5707a
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-02 12:14:40 +0900

    8강3

---
+++ src/main/java/com/ll/domain/system/controller/SystemController/SystemController.java
@@ -0,0 +1,7 @@
+package com.ll.domain.system.controller.SystemController;
+
+public class SystemController {
+    public static void actionExit(){
+        System.out.println("명언 앱을 종료합니다.");
+    }
+}
