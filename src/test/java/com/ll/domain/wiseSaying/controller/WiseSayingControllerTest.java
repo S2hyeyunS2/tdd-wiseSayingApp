8a611983a13bff588c8dd5ee409a828709f0a993
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-02 10:13:01 +0900

    7강

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,12 +1,8 @@
 package com.ll.domain.wiseSaying.controller;
 
-import com.ll.App.App;
-import com.ll.standard.util.TestUtil;
+import com.ll.AppTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,17 +12,22 @@ public class WiseSayingControllerTest {
     @DisplayName("== 명언 앱 ==")
 
     public void t1(){
-        Scanner scanner = TestUtil.getScanner("종료");
-        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
-
-        App app=new App(scanner);
-        app.run();
-
-        String output=outputStream.toString();
-
-        TestUtil.clearSetOutToByteArray(outputStream);
+        String output= AppTest.run("종료");
 
         assertThat(output)
                 .contains("== 명언 앱 ==");
+    }
+
+    //실패한 테스트 1개 만듦
+    @Test
+    @DisplayName("명령) ")
+    public void t2(){
+        String output=AppTest.run("""
+                목록
+                종료
+                """);
+
+        assertThat(output)
+                .contains("명령) ");
     }
 }
