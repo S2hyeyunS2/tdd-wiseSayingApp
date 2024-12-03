1a8a1b360c1712a43ab6c3b71d3479fee58b6dd1
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-03 16:49:57 +0900

    9강1

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -34,14 +34,29 @@ public class WiseSayingControllerTest {
     //실패한 테스트 1개 만듦
     @Test
     @DisplayName("명령을 2번 이상 입력할 수 있습니다. ")
-    public void t3(){
-        String output=AppTest.run("""
+    public void t3() {
+        String output = AppTest.run("""
                 목록
                 목록
                 종료
                 """);
 
-        String[] split=output.split("명령\\)");
+        String[] split = output.split("명령\\)");
         assertThat(split).hasSize(4);
     }
+
+        //실패한 테스트 1개 만듦
+        @Test
+        @DisplayName("등록을 입력하면 내용과 작가를 입력받는다. ")
+        public void t4() {
+            String output = AppTest.run("""
+                    등록
+                    현재를 사랑하라.
+                    작자미상
+                    """);
+
+            assertThat(output)
+                    .contains("명언 : ")
+                    .contains("작가 : ");
+        }
 }
