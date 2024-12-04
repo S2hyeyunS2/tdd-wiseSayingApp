dd1d0977d1724d023ea2eb35956b50b9978ae5fc
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-04 09:10:56 +0900

    10강1

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -45,18 +45,32 @@ public class WiseSayingControllerTest {
         assertThat(split).hasSize(4);
     }
 
-        //실패한 테스트 1개 만듦
-        @Test
-        @DisplayName("등록을 입력하면 내용과 작가를 입력받는다. ")
-        public void t4() {
-            String output = AppTest.run("""
+    //실패한 테스트 1개 만듦
+    @Test
+    @DisplayName("등록을 입력하면 내용과 작가를 입력받는다. ")
+    public void t4() {
+        String output = AppTest.run("""
                     등록
                     현재를 사랑하라.
                     작자미상
                     """);
 
-            assertThat(output)
-                    .contains("명언 : ")
-                    .contains("작가 : ");
-        }
+        assertThat(output)
+                .contains("명언 : ")
+                .contains("작가 : ");
+    }
+
+    //실패한 테스트 1개 만듦
+    @Test
+    @DisplayName("등록이 완료되면 명언번호가 출력된다. ")
+    public void t5() {
+        String output = AppTest.run("""
+                    등록
+                    현재를 사랑하라.
+                    작자미상
+                    """);
+
+        assertThat(output)
+                .contains("1번 명언이 등록되었습니다.");
+    }
 }
