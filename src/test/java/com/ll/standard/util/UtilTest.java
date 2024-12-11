741a0b65fd56e9920e163fb263172b545dcbf23f
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-11 11:29:43 +0900

    f3 b

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -8,32 +8,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UtilTest {
     @Test
     @DisplayName("파일을 생성할 수 있다.")
-    public void t1(){
-        String filePath="test.txt";
+    public void t1() {
+        // given
+        String filePath = "temp/test.txt";
 
+        // when
         Util.file.touch(filePath);
 
-        assertThat(Util.file.exists(filePath)).isTrue();
+        // then
+        assertThat(
+                Util.file.exists(filePath)
+        ).isTrue();
+
+        Util.file.delete(filePath);
     }
 
     @Test
     @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
-    public void t2(){
-        String filePath="test.txt";
+    public void t2() {
+        // given
+        String filePath = "temp/test.txt";
 
-        Util.file.set(filePath,"내용");
+        // when
+        Util.file.set(filePath, "내용");
 
-        assertThat(Util.file.get(filePath, "")).isEqualTo("내용");
+        // then
+        assertThat(
+                Util.file.get(filePath, "")
+        ).isEqualTo("내용");
+
+        Util.file.delete(filePath);
     }
 
     @Test
     @DisplayName("파일을 삭제할 수 있다.")
-    public void t3(){
-        String filePath="test.txt";
+    public void t3() {
+        // given
+        String filePath = "temp/test.txt";
 
+        // when
         Util.file.touch(filePath);
         Util.file.delete(filePath);
 
-        assertThat(Util.file.notExists(filePath)).isTrue();
+        // then
+        assertThat(
+                Util.file.notExists(filePath)
+        ).isTrue();
     }
 }
